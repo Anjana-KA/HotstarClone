a8cc358aab5bdc2b835b9e0627e7d4e55c657d1e
Anjana-KA <karanamkoteanjanaka@gmail.com>
2024-05-15 13:37:19 +0530

    Home Page

--- hotstar/src/components/Detailedpage2.jsx
+++ hotstar/src/components/Detailedpage2.jsx
@@ -19,7 +19,13 @@ const Detailedpage2 = () => {
   }, [id]);
   return (
     <div>
-      <h2 className='title1' alt="text">{imageUrls.title}</h2>
+      <h2 className='title1' alt="text">{imageUrls.title}
+        <p className='pa2'>2023 . 1h 34m . English . </p>
+        <p className='pa1'>Asha, a sharp-witted idealist, makes a powerful wish that's <br></br>answered by Star, a ball of boundless energy. </p>
+        <p className='pa3'>Musical | Comedy | Action | Adventure</p>
+        <button className="subscribe-btn1  "> <span className="play-icon">&#9654;</span>  &nbsp;Subscribe to Watch </button>
+        <button className="plus-bt"> <span className="plus">&nbsp; +</span>  &nbsp; </button>
+      </h2>
       {imageUrls &&
         <img src={imageUrls.posterURL} className='img-con' />
       }
